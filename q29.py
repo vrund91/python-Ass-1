23a3eb29533613f69bdd538ef923c2ff493d6c78
Vrund Patel <vrundpatel876@gmail.com>
2024-07-24 16:26:18 +0530

    Update q29.py

--- q29.py
+++ q29.py
@@ -2,3 +2,28 @@
 operations:
   Slicing operation
   Concatenate by using + and , operator'''
+
+str1 = input("Enter a string:")
+
+print("1.Slicing of string")
+print("2.Concatenation using +")
+print("3.Concatenation using ,")
+
+c = int(input("Enter a choice:"))
+
+if(c==1):
+  r1 = int(input("Enter range1 for slicing:"))
+  r2 = int(input("Enter range2 for slicing::"))
+  print("Sliced string is:",str1[r1:r2])
+ 
+elif(c==2):
+    str2 = input("Enter string to append:")
+    concatstr = str1 + str2
+    print("Concatenated string is:",concatstr)
+
+elif(c==3):
+    str3 = input("Enter string to append:")
+    print("Concatenated string is:",str1,str3)
+    
+else:
+    print("Invalid choice")
