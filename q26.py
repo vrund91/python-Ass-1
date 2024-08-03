3a755a91ae29e361f3043f2247d202b2bc7a818b
Vrund Patel <vrundpatel876@gmail.com>
2024-08-03 23:14:33 +0530

    Update q26.py

--- q26.py
+++ q26.py
@@ -13,27 +13,28 @@ print("3. Logical Operation")
 print("4. Membership Operator Operation")
 print("5. Exit")
 
-choice = input("Enter your choice: ")
-if choice == '1':
+choice = int(input("Enter your choice: "))
+
+if choice == 1:
         print("1. Addition")
         print("2. Subtraction")
         print("3. Multiplication")
         print("4. Division")
-        arithmetic_choice = input("Enter your choice: ")
+        arithmetic_choice = int(input("Enter your choice: "))
 
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
-        if arithmetic_choice == '1':
+        if arithmetic_choice == 1:
             result = num1 + num2
             print("Result:", result)
-        elif arithmetic_choice == '2':
+        elif arithmetic_choice == 2:
             result = num1 - num2
             print("Result:", result)
-        elif arithmetic_choice == '3':
+        elif arithmetic_choice == 3:
             result = num1 * num2
             print("Result:", result)
-        elif arithmetic_choice == '4':
+        elif arithmetic_choice == 4:
             if num2 == 0:
                 print("Error! Division by zero.")
             else:
@@ -42,7 +43,7 @@ if choice == '1':
         else:
             print("Invalid choice")
 
-elif choice == '2':
+elif choice == 2:
        var1='Hello'
        var2='Hello'
        var3='World'
@@ -53,31 +54,34 @@ elif choice == '2':
        print(var4 is var5)
        print(var3 is not var2)
 
-elif choice == '3':
+elif choice == 3:
         print("1. AND")
         print("2. OR")
         print("3. NOT")
-        logical_choice = input("Enter your choice: ")
 
-        if logical_choice == '1':
+        logical_choice = int(input("Enter your choice: "))
+
+        if logical_choice == 1:
             bool1 = bool(input("Enter first boolean: "))
             bool2 = bool(input("Enter second boolean: "))
             result = bool1 and bool2
             print("Result:", result)
-        elif logical_choice == '2':
+
+        elif logical_choice == 2:
             bool1 = bool(input("Enter first boolean : "))
             bool2 = bool(input("Enter second boolean : "))
             result = bool1 or bool2
             print("Result:", result)
-        elif logical_choice == '3':
+
+        elif logical_choice == 3:
             bool1 = bool(input("Enter a boolean: "))
             result = not bool1
             print("Result:", result)
+
         else:
             print("Invalid choice")
 
-   
-elif choice == '4':
+elif choice == 4:
         range_user=["varun","ravi","amrit","nitin"]
         name=input("Enter your name:")
 
@@ -86,8 +90,8 @@ elif choice == '4':
         else:
              print(name in range_user,"Access denied")
 
-elif choice == '5':
+elif choice == 5:
         print("Exit")
 
 else:
-        print("Invalid choice. Please enter a valid option.")
+     print("Invalid choice")
